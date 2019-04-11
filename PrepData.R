397c029982622383f5ebdfc85ec74a513c39c11c
Alexander Mohn <pogrmman@gmail.com>
2019-04-11 09:24:48 -0500

    Initial Commit

---
+++ PrepData.R
@@ -0,0 +1,181 @@
+### Libraries ###
+library(dplyr)
+library(tidyr)
+
+# Load data
+noTaxnoTip <- read.csv("./Data/no_tax_no_tip.csv", skip = 4, header = TRUE)[1:21,]
+withTaxTip <- read.csv("./Data/tax_tip_inc.csv", skip = 4, header = TRUE)[1:21,]
+
+# Convert all to numeric
+noTaxnoTip <- noTaxnoTip %>% mutate_all(function(x) as.numeric(as.character(gsub(",","",x))))
+withTaxTip <- withTaxTip %>% mutate_all(function(x) as.numeric(as.character(gsub(",","",x))))
+
+# Convert all from 1988 dollars to 2017 dollars -- $1 in 1988 = $2.09 in 2017
+noTaxnoTip <- noTaxnoTip %>% mutate_at(vars(-Year), function(x) x * 2.09)
+withTaxTip <- withTaxTip %>% mutate_at(vars(-Year), function(x) x * 2.09)
+
+# Calculate tips and taxes
+TaxesTips <- withTaxTip - noTaxnoTip
+TaxesTips$Year <- withTaxTip$Year
+rm(withTaxTip)
+
+# Seperate into alcohol and food away from and at home
+alcoholAtHome <- noTaxnoTip %>% 
+  select(Year, 
+         "Liquor Stores" = Liquor.stores, 
+         "Food Stores" = Food.stores, 
+         Other = Other.AAH.sales..NEC, 
+         Total = Total.AAH) %>% gather("Location", "Sales", 2:5)
+alcoholAwayFromHome <- noTaxnoTip %>%
+  select(Year, 
+         "Eating/Drinking Places" = Eating.and.drinking.places, 
+         "Hotels/Motels" = Hotels.and.motels.1, 
+         Other = Other.AAFH..NEC, 
+         Total = Total.AAFH) %>% gather("Location", "Sales", 2:5)
+foodAtHome <- noTaxnoTip %>% 
+  select(Year, 
+         "Grocery Stores" = Grocery.stores, 
+         "Convenience Stores" = Convenience.stores, 
+         "Other Food Stores" = Other.food.stores, 
+         "Warehouse Clubs/Supercenters" = Warehouse.clubs.and.supercenters,
+         "Mass Merchandisers" = Mass.merchandisers, 
+         "Other Stores and Food Services" = Other.stores.and.foodservice,
+         "Mail Order/Home Delivery" = Mail.order.and.home.delivery, 
+         "Direct Sales from Producers" = Direct.selling.by.farmers..manufacturers.and.wholesalers, 
+         "Home Production/Donations" = Home.production.and.donations,
+         Total = Total.FAH) %>% gather("Location", "Sales", 2:11)
+foodAwayFromHome <- noTaxnoTip %>%
+  select(Year, 
+         "Full Service Restaurants" = Full.service.restaurants, 
+         "Limited Service Restaurants" = Limited.service.restaurants, 
+         "Drinking Establishments" = Drinking.places, 
+         "Hotels/Motels" = Hotels.and.motels, 
+         "Retail Stores/Vending" = Retail.stores.and.vending, 
+         "Recreational Places" = Recreational.places, 
+         "Schools/Colleges" = Schools.and.colleges, 
+         Other = Other.FAFH.sales..NEC, 
+         "Food Furnished/Donated" = Food.furnished.and.donated, 
+         Total = Total.FAFH) %>% gather("Location", "Sales", 2:11)
+
+# Add taxes and tip info
+alcoholAtHomeTaxesTips <- TaxesTips %>% 
+  select(Year, 
+         "Liquor Stores" = Liquor.stores, 
+         "Food Stores" = Food.stores, 
+         Other = Other.AAH.sales..NEC, 
+         Total = Total.AAH) %>% gather("Location", "Tips.Taxes", 2:5)
+alcoholAwayFromHomeTaxesTips <- TaxesTips %>%
+  select(Year, 
+         "Eating/Drinking Places" = Eating.and.drinking.places, 
+         "Hotels/Motels" = Hotels.and.motels.1, 
+         Other = Other.AAFH..NEC, 
+         Total = Total.AAFH) %>% gather("Location", "Tips.Taxes", 2:5)
+foodAtHomeTaxesTips <- TaxesTips %>% 
+  select(Year, 
+         "Grocery Stores" = Grocery.stores, 
+         "Convenience Stores" = Convenience.stores, 
+         "Other Food Stores" = Other.food.stores, 
+         "Warehouse Clubs/Supercenters" = Warehouse.clubs.and.supercenters,
+         "Mass Merchandisers" = Mass.merchandisers, 
+         "Other Stores and Food Services" = Other.stores.and.foodservice,
+         "Mail Order/Home Delivery" = Mail.order.and.home.delivery, 
+         "Direct Sales from Producers" = Direct.selling.by.farmers..manufacturers.and.wholesalers, 
+         "Home Production/Donations" = Home.production.and.donations,
+         Total = Total.FAH) %>% gather("Location", "Tips.Taxes", 2:11)
+foodAwayFromHomeTaxesTips <- TaxesTips %>%
+  select(Year, 
+         "Full Service Restaurants" = Full.service.restaurants, 
+         "Limited Service Restaurants" = Limited.service.restaurants, 
+         "Drinking Establishments" = Drinking.places, 
+         "Hotels/Motels" = Hotels.and.motels, 
+         "Retail Stores/Vending" = Retail.stores.and.vending, 
+         "Recreational Places" = Recreational.places, 
+         "Schools/Colleges" = Schools.and.colleges, 
+         Other = Other.FAFH.sales..NEC, 
+         "Food Furnished/Donated" = Food.furnished.and.donated, 
+         Total = Total.FAFH) %>% gather("Location", "Tips.Taxes", 2:11)
+
+foodAtHome <- foodAtHome %>% left_join(foodAtHomeTaxesTips, by=c("Year", "Location"))
+foodAwayFromHome <- foodAwayFromHome %>% left_join(foodAwayFromHomeTaxesTips, 
+                                                   by=c("Year", "Location"))
+alcoholAtHome <- alcoholAtHome %>% left_join(alcoholAtHomeTaxesTips, by=c("Year", "Location"))
+alcoholAwayFromHome <- alcoholAwayFromHome %>% left_join(alcoholAwayFromHomeTaxesTips,
+                                                         by=c("Year", "Location"))
+
+rm(foodAtHomeTaxesTips, foodAwayFromHomeTaxesTips, 
+   alcoholAtHomeTaxesTips, alcoholAwayFromHomeTaxesTips)
+
+# Sum taxes and tips
+alcoholAtHome <- alcoholAtHome %>% mutate(SalesWithTaxTip = Sales + Tips.Taxes)
+alcoholAwayFromHome <- alcoholAwayFromHome %>% mutate(SalesWithTaxTip = Sales + Tips.Taxes)
+foodAtHome  <- foodAtHome %>% mutate(SalesWithTaxTip = Sales + Tips.Taxes)
+foodAwayFromHome <- foodAwayFromHome %>% mutate(SalesWithTaxTip = Sales + Tips.Taxes)
+
+# Get total food expenditures
+totals <- alcoholAtHome %>% bind_rows(alcoholAwayFromHome) %>% 
+  bind_rows(foodAtHome) %>% bind_rows(foodAwayFromHome) %>% filter(Location == "Total") %>%
+  group_by(Year) %>% summarize(TotalNoTipsTaxes = sum(Sales),
+                               TotalTipsTaxes = sum(Tips.Taxes),
+                               Total = sum(SalesWithTaxTip))
+
+# Calculate percentages
+alcoholAtHome <- alcoholAtHome %>% left_join(totals, by = "Year") %>%
+  mutate(PercentSales = Sales * 100 / TotalNoTipsTaxes,
+         PercentTipsTaxes = Tips.Taxes * 100 / TotalTipsTaxes,
+         PercentTotal = SalesWithTaxTip * 100 / Total) %>% select(-TotalNoTipsTaxes,
+                                                                  -TotalTipsTaxes,
+                                                                  -Total)
+alcoholAwayFromHome <- alcoholAwayFromHome %>% left_join(totals, by = "Year") %>%
+  mutate(PercentSales = Sales * 100 / TotalNoTipsTaxes,
+         PercentTipsTaxes = Tips.Taxes * 100 / TotalTipsTaxes,
+         PercentTotal = SalesWithTaxTip * 100 / Total) %>% select(-TotalNoTipsTaxes,
+                                                                  -TotalTipsTaxes,
+                                                                  -Total)
+foodAtHome <- foodAtHome %>% left_join(totals, by = "Year") %>%
+  mutate(PercentSales = Sales * 100 / TotalNoTipsTaxes,
+         PercentTipsTaxes = Tips.Taxes * 100 / TotalTipsTaxes,
+         PercentTotal = SalesWithTaxTip * 100 / Total) %>% select(-TotalNoTipsTaxes,
+                                                                  -TotalTipsTaxes,
+                                                                  -Total)
+foodAwayFromHome <- foodAwayFromHome %>% left_join(totals, by = "Year") %>%
+  mutate(PercentSales = Sales * 100 / TotalNoTipsTaxes,
+         PercentTipsTaxes = Tips.Taxes * 100 / TotalTipsTaxes,
+         PercentTotal = SalesWithTaxTip * 100 / Total) %>% select(-TotalNoTipsTaxes,
+                                                                  -TotalTipsTaxes,
+                                                                  -Total)
+
+# Get list of locations in each type
+foodAtHomeChoices <- foodAtHome %>% select(Location) %>% 
+  distinct() %>% filter(Location != "Total")
+foodAwayFromHomeChoices <- foodAwayFromHome %>% select(Location) %>% 
+  distinct() %>% filter(Location != "Total")
+alcoholAtHomeChoices <- alcoholAtHome %>% select(Location) %>%
+  distinct() %>% filter(Location != "Total")
+alcoholAwayFromHomeChoices <- alcoholAwayFromHome %>% select(Location) %>%
+  distinct() %>% filter(Location != "Total")
+
+# Make both away and at home versions
+bothAlcohol <- alcoholAwayFromHome %>% bind_rows(alcoholAtHome)
+totals <- bothAlcohol %>% filter(Location == "Total") %>% group_by(Year) %>%
+  summarize(Sales = sum(Sales), Tips.Taxes = sum(Tips.Taxes), 
+            SalesWithTaxTip = sum(SalesWithTaxTip), PercentSales = sum(PercentSales),
+            PercentTipsTaxes = sum(PercentTipsTaxes), PercentTotal = sum(PercentTotal))
+totals$Location = "Total"
+bothAlcohol <- bothAlcohol %>% filter(Location != "Total") %>% bind_rows(totals)
+totals <- bothAlcohol %>% filter(Location == "Other") %>% group_by(Year) %>%
+  summarize(Sales = sum(Sales), Tips.Taxes = sum(Tips.Taxes), 
+            SalesWithTaxTip = sum(SalesWithTaxTip), PercentSales = sum(PercentSales),
+            PercentTipsTaxes = sum(PercentTipsTaxes), PercentTotal = sum(PercentTotal))
+totals$Location = "Other"
+bothAlcohol <- bothAlcohol %>% filter(Location != "Other") %>% bind_rows(totals)
+
+bothFood <- foodAwayFromHome %>% bind_rows(foodAtHome)
+totals <- bothFood %>% filter(Location == "Total") %>% group_by(Year) %>%
+  summarize(Sales = sum(Sales), Tips.Taxes = sum(Tips.Taxes), 
+            SalesWithTaxTip = sum(SalesWithTaxTip), PercentSales = sum(PercentSales),
+            PercentTipsTaxes = sum(PercentTipsTaxes), PercentTotal = sum(PercentTotal))
+totals$Location = "Total"
+bothFood <- bothFood %>% filter(Location != "Total") %>% bind_rows(totals)
+
+bothAlcoholChoices <- alcoholAtHomeChoices %>% bind_rows(alcoholAwayFromHomeChoices)
+bothFoodChoices <- foodAtHomeChoices %>% bind_rows(foodAwayFromHomeChoices)
---
+++ app.R
@@ -0,0 +1,146 @@
+### Libraries ###
+library(shiny)
+library(ggplot2)
+library(gridExtra)
+library(stringr)
+
+source("PrepData.R")
+
+ui <- fluidPage(
+  titlePanel("US Food and Alcohol Purchases"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons("Food.Alc",
+                   label = "Food/Alcohol",
+                   choices = list("Food", "Alcohol"),
+                   selected = "Food"),
+      radioButtons("Home.Away",
+                   label = "Consumed At Home/Away from Home",
+                   choices = list("Home", "Away from Home", "Both"),
+                   selected = "Home"),
+      selectInput("Locations",
+                  label = "Data from what locations?",
+                  choices = foodAtHomeChoices,
+                  multiple = TRUE,
+                  selected = NULL),
+      checkboxInput("Percent",
+                    label = "Show as percentages?"),
+      checkboxInput("Tax.Tip",
+                   label = "Include taxes and tips?"),
+      checkboxInput("Tax.Tip.Separate",
+                    label = "Show taxes and tips on another graph?")
+    ),
+    
+    mainPanel(plotOutput("plot", height = "500"))
+))
+  
+server <- function(input, output, session) {
+  
+  active <- reactive({
+    if (input$Food.Alc == "Food") {
+      type <- "food"
+    } else if (input$Food.Alc == "Alcohol") {
+      type <- "alcohol"
+    }
+    if (input$Home.Away == "Home") {
+      home <- "home"
+    } else if (input$Home.Away == "Away from Home") {
+      home <- "away"
+    } else if (input$Home.Away == "Both") {
+      home <- "both"
+    }
+    
+    if (type == "food" & home == "home") {
+      updateSelectInput(session, "Locations", choices = foodAtHomeChoices)
+      active <- foodAtHome
+    } else if (type == "food" & home == "away") {
+      updateSelectInput(session, "Locations", choices = foodAwayFromHomeChoices)
+      active <- foodAwayFromHome
+    } else if (type == "food" & home == "both") {
+      updateSelectInput(session, "Locations", choices = bothFoodChoices)
+      active <- bothFood
+    } else if (type == "alcohol" & home == "home") {
+      updateSelectInput(session, "Locations", choices = alcoholAtHomeChoices)
+      active <- alcoholAtHome
+    } else if (type == "alcohol" & home == "away") {
+      updateSelectInput(session, "Locations", choices = alcoholAwayFromHomeChoices)
+      active <- alcoholAwayFromHome
+    } else if (type == "alcohol" & home == "both") {
+      updateSelectInput(session, "Locations", choices = bothAlcoholChoices)
+      active <- bothAlcohol
+    }
+    return(active)
+  })
+  
+  output$plot <- renderPlot({
+    active <- active()
+    totals <- active %>% filter(Location == "Total")
+    if (input$Tax.Tip & !input$Percent) {
+      yvar = "SalesWithTaxTip"
+      figTitle = "Sales in Millions of 2017 Dollars"
+      yTitle = "Sales (Millions USD)"
+      ttTitle = "Tips and Taxes in Millions of 2017 Dollars"
+      tty = "Tips.Taxes"
+    } else if(input$Tax.Tip & input$Percent) {
+      yvar = "PercentTotal"
+      figTitle = "Percentage of Total Food and Alcohol Expenditures"
+      yTitle = "Percentage of Expenditures"
+      ttTitle = "Percentage of Total Food/Alcohol Tips and Taxes"
+      tty = "PercentTipsTaxes"
+    } else if(input$Percent) {
+      yvar = "PercentSales"
+      figTitle = "Percentage of Total Food and Alcohol Expenditures"
+      yTitle = "Percentage of Expenditures"
+      ttTitle = "Percentage of Total Food/Alcohol Tips and Taxes"
+      tty = "PercentTipsTaxes"
+    } else {
+      yvar = "Sales"
+      figTitle = "Sales in Millions of 2017 Dollars"
+      yTitle = "Sales (Millions USD)"
+      ttTitle = "Tips and Taxes in Millions of 2017 Dollars"
+      tty = "Tips.Taxes"
+    }
+    
+    totalsPlot <- ggplot(totals, aes(x = Year, y = get(yvar))) +
+      geom_point(size = 5, shape = "square") +
+      labs(title = figTitle,
+           x = "Year",
+           y = yTitle)
+    
+    if (!is.null(input$Locations)) {
+      active <- active %>% filter(Location %in% input$Locations)
+      specificPlot <- ggplot(active, aes(x = Year, y = get(yvar), color = Location)) +
+        geom_point(size = 5, shape = "square") + 
+        labs(title = figTitle,
+             x = "Year",
+             y = yTitle)
+    } else { specificPlot <- NULL }
+    
+    if (input$Tax.Tip.Separate & !is.null(specificPlot)) {
+      taxTipPlot <- ggplot(active, aes(x = Year, y = get(tty), color = Location)) +
+        geom_point(size = 5, shape = "square") +
+        labs(title = ttTitle,
+             x = "Year",
+             y = yTitle)
+    } else if (input$Tax.Tip.Separate) {
+      taxTipPlot <- ggplot(totals, aes(x = Year, y = get(tty))) +
+        geom_point(size = 5, shape = "square") +
+        labs(title = ttTitle,
+             x = "Year",
+             y = yTitle)
+    } else { taxTipPlot <- NULL }
+    
+    if (!is.null(specificPlot) & ! is.null(taxTipPlot)) {
+      grid.arrange(specificPlot, taxTipPlot)
+    } else if (!is.null(specificPlot)) {
+      grid.arrange(specificPlot)
+    } else if (!is.null(taxTipPlot)) {
+      grid.arrange(totalsPlot, taxTipPlot)
+    } else {
+      grid.arrange(totalsPlot)
+    }
+  })
+}
+
+shinyApp(ui, server)
